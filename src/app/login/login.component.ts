555eaca8754415e4ee0d8be19283b110c75b82c5
PC-CMONROY\Cmonroyp <carlosmonroy@outlook.com>
2018-04-15 21:22:41 -0500

    Validaciones en login y register

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,11 +1,11 @@
+import { DashboardComponent } from './../layout/dashboard/dashboard.component';
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { routerTransition } from '../router.animations';
+import { FormGroup, FormControl, Validators, FormArray } from '@angular/forms'
 
-
+//Arreglo de background de imagenes
 import { CAMBIA_IMAGEN } from './../models/bg.images';
-
-
 
 @Component({
     selector: 'app-login',
@@ -15,6 +15,8 @@ import { CAMBIA_IMAGEN } from './../models/bg.images';
 })
 export class LoginComponent implements OnInit {
 
+    forma: FormGroup;
+
     imagen:any[] =[];
     bgImages:any[] = CAMBIA_IMAGEN;
 
@@ -22,12 +24,20 @@ export class LoginComponent implements OnInit {
     }
 
     ngOnInit() {
-        let  img =  Math.floor(Math.random() * 7) + 1
+        let  img =  Math.floor(Math.random() * 6) + 1
         this.cambiaImage(img)
+
+        this.validacionCampos();
     }
 
-    onLoggedin() {
-        localStorage.setItem('isLoggedin', 'true');
+    validacionCampos(){
+        this.forma = new FormGroup({
+            email: new FormControl('',[
+                                        Validators.required,
+                                        Validators.email
+                                    ]),
+           password: new FormControl('',[Validators.required])
+        })
     }
 
     cambiaImage(n){
@@ -40,5 +50,22 @@ export class LoginComponent implements OnInit {
         //         url: `${this.bgImages[n].nombre}`,
         //         });
 
+    }
+
+    logIn(){
+
+        if(this.forma.invalid){
+            return; 
+          }
+
+        localStorage.setItem('isLoggedin', 'true');
+
+        console.log('valores', this.forma.value );  
+        console.log('forma Completa', this.forma );
+        console.log('Valido', this.forma.valid );
+        console.log('controles',this.forma.controls)
+
+        //redirecciona al Dashboard
+        this.router.navigate(['/dashboard']);
     }
 }
--- src/app/signup/signup.component.ts
+++ src/app/signup/signup.component.ts
@@ -1,5 +1,11 @@
 import { Component, OnInit } from '@angular/core';
 import { routerTransition } from '../router.animations';
+import { FormGroup, FormControl, Validators, FormArray } from '@angular/forms'
+
+//Arreglo de background de imagenes
+import { CAMBIA_IMAGEN } from './../models/bg.images';
+//model
+import { Usuario } from '../models/usuario';
 
 @Component({
     selector: 'app-signup',
@@ -8,7 +14,73 @@ import { routerTransition } from '../router.animations';
     animations: [routerTransition()]
 })
 export class SignupComponent implements OnInit {
+
+    forma: FormGroup;
+
+    usuario: Usuario
+    imagen:any[] =[];
+    bgImages:any[] = CAMBIA_IMAGEN;
+
+    sonIguales( campo1: string, campo2: string){
+
+        return (group: FormGroup)=>{
+    
+          let pass1 = group.controls[campo1].value;
+          let pass2 = group.controls[campo2].value;
+    
+          if(pass1 === pass2){
+            return null //pasa la validacion
+          }
+          //error no pasa la validacion
+          return {
+            sonIguales: true
+          }
+        }
+      }
+
     constructor() {}
 
-    ngOnInit() {}
+    ngOnInit() {
+        this.cambiaImage();
+        this.validacionCampos();
+    }
+
+    validacionCampos(){
+
+        this.forma = new FormGroup({
+            nombre: new FormControl('',[Validators.required]),
+            email: new FormControl('',[Validators.required, Validators.email]),
+            pwd1: new FormControl('', [Validators.required]),
+            pwd2: new FormControl('', [Validators.required])
+        },
+        { validators: this.sonIguales('pwd1','pwd2') }
+        )
+    }
+
+    cambiaImage(){
+        //se deja como arreglo por si se quiere, generar imagenes aleatorias como en el login.
+        this.imagen = [
+              ... this.imagen,{url: `${this.bgImages[4].nombre}`}
+       ] 
+     }
+
+     singIn(){
+
+        if(this.forma.invalid){
+            return; 
+          }
+
+        console.log('valores', this.forma.value );  
+        console.log('forma Completa', this.forma );
+        console.log('Valido', this.forma.valid );
+        console.log('controles',this.forma.controls)
+
+        this.usuario = new Usuario(
+            this.forma.value.nombre,
+            this.forma.value.email,
+            this.forma.value.pwd1
+        )
+
+        console.log('resultado',this.usuario)
+     }
 }
